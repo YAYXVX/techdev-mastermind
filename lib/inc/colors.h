8bcd62e735a1acaffc1edcd297ad5d89371a73b8
Carlos Martin <carlos.martin-del-rio@etu.unistra.fr>
2024-03-22 13:55:56 +0100

    Add libcolor functions

---
+++ lib/inc/colors.h
@@ -0,0 +1,7 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+void print_color(const char *text, char color);
+
+#endif // COLORS_H
+
---
+++ lib/src/colors.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "colors.h"
+
+void print_color(const char *text, const char color) {
+    switch (color) {
+        case 'R':
+            printf("\033[31m%s\033[0m", text); // Red
+            break;
+        case 'G':
+            printf("\033[32m%s\033[0m", text); // Green
+            break;
+        case 'Y':
+            printf("\033[33m%s\033[0m", text); // Yellow
+            break;
+        case 'B':
+            printf("\033[34m%s\033[0m", text); // Blue
+            break;
+        case 'P':
+            printf("\033[35m%s\033[0m", text); // Purple
+            break;
+        case 'C':
+            printf("\033[36m%s\033[0m", text); // Cyan
+            break;
+        default:
+            printf("%s", text); // Default color
+    }
+}
+
