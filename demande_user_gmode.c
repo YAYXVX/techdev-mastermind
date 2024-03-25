788b4d44eef5a53bb3789ec5d823ca071f9ebf95
Yagiz Ayvaci <yagiz.ayvaci@etu.unistra.fr>
2024-03-26 00:36:54 +0100

    updated the user input dialog

--- demande_user_gmode.c
+++ demande_user_gmode.c
@@ -7,13 +7,22 @@
  
 #include <stdio.h>
 
-int main(){
-  printf("Bienvenu au jeu MASTERMIND!!! \nSouhaitez-vous jouer en mode HARD ou EASY ? \n");
-  char mode;
-  scanf("%c", &mode);
-  if (mode == 'H'){ 
-    printf("Vous avez choisi le mode HARD. \n");
-  } else {
-    printf("Vous avez choisi le mode EASY. \n");
-  }
+int main() {
+    printf("Bienvenu au jeu MASTERMIND!!!\n");
+    char mode;
+    do {
+        printf("Souhaitez-vous jouer en mode HARD ou EASY ? \n");
+        scanf(" %c", &mode); // Add a space before %c to consume leading whitespace characters
+
+        if (mode == 'H' || mode == 'h') {
+            printf("Vous avez choisi le mode HARD \n");
+        } else if (mode == 'E' || mode == 'e') {
+            printf("Vous avez choisi le mode EASY \n");
+        } else {
+            printf("Vous n'avez pas choisi un mode valide \n");
+        } 
+    } while (mode != 'H' && mode != 'h' && mode != 'E' && mode != 'e');
+    
+    return 0;
 }
+
