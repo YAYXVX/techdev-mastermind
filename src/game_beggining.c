744870d711ce057fec3b1dfd45021861989566f2
MARTIN-DEL-RIO CARLOS <carlos.martin-del-rio@etu.unistra.fr>
2024-04-07 19:49:53 +0200

    Update game_beggining.c for reading from a file

--- src/game_beggining.c
+++ src/game_beggining.c
@@ -11,6 +11,7 @@
 #include <string.h> // for strcpy and strcmp
 #include "game_beggining.h"
 #include "random.h"
+#include "user_input.h"
 #include "evaluation.h"
 #define NUM_COLORS 6
 
@@ -21,12 +22,12 @@ int gamebeggining() {
     // Seed the random number generator
     srand(time(NULL));
 
-    // Generate a random sequence of 4 letters
+    // Generate a random sequence of 4 letters based on user choice
     printf("Bienvenu au jeu MASTERMIND!!!\n");
-    char mode[10];
+    char mode[10]; // Increased size to accommodate "fichier" option
     do {
-        printf("Souhaitez-vous jouer en mode HARD ou EASY ? \n");
-        scanf(" %s", mode);
+        printf("Souhaitez-vous jouer en mode HARD, EASY, ou à partir d'un fichier ? \n>");
+        scanf("%s", mode);
 
         // Clear the input buffer
         while (getchar() != '\n'); // Consume characters until newline is encountered
@@ -38,10 +39,13 @@ int gamebeggining() {
         } else if (strcasecmp(mode, "EASY") == 0) {
             printf("Vous avez choisi le mode EASY \n");
             generateRandomSequence_EASY(sequence, 4);
+        } else if (strcasecmp(mode, "fichier") == 0) {
+            printf("Vous avez choisi de lire le code à partir d'un fichier.\n");
+            read_code_from_file(sequence);
         } else {
-            printf("Vous n'avez pas choisi un mode valide \n");
+            printf("Mode invalide. Veuillez choisir HARD, EASY, ou fichier.\n");
         }
-    } while ((strcasecmp(mode, "HARD") != 0) && (strcasecmp(mode, "EASY") != 0));
+    } while ((strcasecmp(mode, "HARD") != 0) && (strcasecmp(mode, "EASY") != 0) && (strcasecmp(mode, "fichier") != 0));
 
     printf("    ▁▁▁▁▁▁▁▁▁▁▁▁\n");
     printf("    ┃GAME START┃\n");
